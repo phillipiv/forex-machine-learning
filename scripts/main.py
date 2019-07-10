00c5cff4e3854fd1632a19b248d6aad45ad509a0
Felipe <ev.galois7@gmail.com>
2019-07-10 01:52:28 -0300

    PEP 8 compliance

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,20 @@
+import argparse
+
 from util import *
 
-import argparse
 
 ##################################################################################
 ##### parse script parameters ####################################################
 ##################################################################################
 
 parser = argparse.ArgumentParser(description='Unsupervised learning approach to Forex trading analysis')
-parser.add_argument('--date_init', nargs='?', help='backtesting initial date',default = '2010-01-01' )
-parser.add_argument('--date_end', nargs='?', help='backtesting end date',default = '2018-01-01' )
-parser.add_argument('--h_init', nargs='?', help='time interval intial hour',default = 9 )
-parser.add_argument('--h_end', nargs='?', help='time interval final hour',default = 17 )
-parser.add_argument('--h_split', nargs='?', help='interval splitting parameter',default = 0.8 )
-parser.add_argument('--n_clusters', nargs='?', help='number of clusters in clustering algorithm',default = 6 )
-parser.add_argument('--hists', nargs='?', help='boolean plot performance distribution histogram',default = "F" )
+parser.add_argument('--date_init', nargs='?', help='backtesting initial date', default='2010-01-01')
+parser.add_argument('--date_end', nargs='?', help='backtesting end date', default='2018-01-01')
+parser.add_argument('--h_init', nargs='?', help='time interval intial hour', default=9)
+parser.add_argument('--h_end', nargs='?', help='time interval final hour', default=17)
+parser.add_argument('--h_split', nargs='?', help='interval splitting parameter', default=0.8)
+parser.add_argument('--n_clusters', nargs='?', help='number of clusters in clustering algorithm', default=6)
+parser.add_argument('--hists', nargs='?', help='boolean plot performance distribution histogram', default="F")
 
 args = parser.parse_args()
 
@@ -21,46 +22,47 @@ date_init = args.date_init
 date_end = args.date_end
 h_init = int(args.h_init)
 h_end = int(args.h_end)
-h_split=float(args.h_split)
-n_clusters=int(args.n_clusters)
-hists=(args.hists in ["T", "t", "true","True", "1"])
+h_split = float(args.h_split)
+n_clusters = int(args.n_clusters)
+hists = args.hists in ["T", "t", "true", "True", "1"]
 
-## data
+# data
 
-data=load_all_data()
+data = load_all_data()
 
-## intervals
+# intervals
 
-time_interval=[h_init,h_end]
+time_interval = [h_init, h_end]
 
-intervals=get_intervals(date_init,date_end,time_interval)
+intervals = get_intervals(date_init, date_end, time_interval)
 
-## data to intervals
+# data to intervals
 
-data_intervals=data_to_intervals(data,intervals)
+data_intervals = data_to_intervals(data, intervals)
 
-## split intervals
+# split intervals
 
-train_intervals,label_intervals=split_intervals(data_intervals,h_split)
+train_intervals, label_intervals = split_intervals(data_intervals, h_split)
 
-## normalize train data
+# normalize train data
 
 normalized_train_intervals = normalize_intervals(train_intervals)
 
-## cluster data
+# cluster data
 
-cluster_indexes=get_cluster_indexes(normalized_train_intervals,n_clusters)
+cluster_indexes = get_cluster_indexes(normalized_train_intervals, n_clusters)
 
-## labels
+# labels
 
-labels=get_labels(label_intervals)
+labels = get_labels(label_intervals)
 
-## performances
+# performances
 
-performances_by_cluster,labels_by_cluster=get_performance(cluster_indexes,labels)
+performances_by_cluster, labels_by_cluster = get_performance(cluster_indexes, labels)
 
-for j,performance in enumerate(performances_by_cluster):
-    print ('Performance Cluster #',j, performance)
-    plot_hist(labels_by_cluster[j], hists,'Cluster #'+str(j)+ ' performance distribution')
+for j, performance in enumerate(performances_by_cluster):
+    print ('Performance Cluster #', j, performance)
+    plot_hist(labels_by_cluster[j], hists, 'Cluster #' + str(j) + ' performance distribution')
 
-if (hists): plt.show()
+if hists:
+    plt.show()
--- scripts/util.py
+++ scripts/util.py
@@ -1,47 +1,53 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
 
 def load_all_data():
 
     return pd.read_pickle('../data/EURUSD-2010_2018-closeAsk.pkl')
 
-def split_intervals(data_intervals,h_split):
+
+def split_intervals(data_intervals, h_split):
 
     output_train = []
     output_label = []
 
     interval_length = np.shape(data_intervals[0])[0]
-    split_idx=int(h_split*interval_length)
+    split_idx = int(h_split*interval_length)
 
-    for j,data_interval in enumerate(data_intervals):
+    for j, data_interval in enumerate(data_intervals):
         output_train.append(data_interval[:split_idx])
         output_label.append(data_interval[split_idx:])
 
-    return output_train,output_label
+    return output_train, output_label
 
-def get_performance(cluster_indexes,labels):
 
-    clusters=np.sort(np.unique(cluster_indexes))
+def get_performance(cluster_indexes, labels):
 
-    output_performance=[]
+    clusters = np.sort(np.unique(cluster_indexes))
+
+    output_performance = []
     output_labels = []
 
     for c in clusters:
-        indxs=np.where(cluster_indexes==c)[0]
-        labels_=np.asarray(labels)[indxs]
+        indxs = np.where(cluster_indexes == c)[0]
+        labels_ = np.asarray(labels)[indxs]
         output_labels.append(labels_)
         output_performance.append(np.sum(labels_))
 
-    return output_performance,output_labels
+    return output_performance, output_labels
 
-def plot_hist(data,hists,title):
 
-    if (hists):
+def plot_hist(data, hists, title):
+
+    if hists:
         fig = plt.figure()
         plt.hist(data,bins=30)
         plt.grid()
         plt.title(title)
+
 
 def normalize_intervals(train_intervals):
 
@@ -54,17 +60,17 @@ def normalize_intervals(train_intervals):
 
     return output
 
-def get_cluster_indexes(normalized_train_intervals,n_clusters):
 
-    from sklearn.cluster import KMeans
+def get_cluster_indexes(normalized_train_intervals, n_clusters):
 
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(normalized_train_intervals)
 
     return kmeans.labels_
 
+
 def get_labels(label_intervals):
 
-    labels=[]
+    labels = []
 
     for data_ in label_intervals:
 
@@ -73,14 +79,15 @@ def get_labels(label_intervals):
     return labels
 
 
-def get_intervals(date_init,date_end,interval):
+def get_intervals(date_init, date_end, interval):
 
-    h_init,h_end=interval
+    h_init, h_end=interval
 
     output = []
     rango_total = pd.date_range(date_init, date_end, freq='D')
+
     for i in rango_total:
-        _=pd.date_range(i,periods=1440, freq='T')
+        _ = pd.date_range(i,periods=1440, freq='T')
         output.append(_[h_init*60:h_end*60])
 
     return output
@@ -88,15 +95,15 @@ def get_intervals(date_init,date_end,interval):
 
 def data_to_intervals(data, intervals):
 
-    interval_length=np.shape(intervals[0])[0]
+    interval_length = np.shape(intervals[0])[0]
 
-    output=[]
+    output = []
 
     for j, interval in enumerate(intervals):
 
         data_ = data[interval]
 
-        if (not(np.isnan(data_).any())) and (np.shape(data_)[0]==interval_length):
+        if (not(np.isnan(data_).any())) and (np.shape(data_)[0] == interval_length):
             output.append(data_)
 
     return output
